7fd2254d55a23573b5a2a74c9a9025ac66cc651b
Giovanni Bedoya <giovedoya@gmail.com>
2023-01-01 17:48:15 +0100

    'Post routes, an create new present'

--- routes/presents.js
+++ routes/presents.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Present = require('../model/present');
 
-/* GET presents page. */
+/* GET all presents page. */
 /* ROUTE presents*/
 router.get('/', async function (req, res, next) {
     try {
@@ -12,7 +12,40 @@ router.get('/', async function (req, res, next) {
         next(error)
     }
 });
+/* GET search */
+/* ROUTE presents/search*/
+router.get('/search', async function (req, res, next){
+    const { name } = req.query;
+    res.render('search', { query: title });
+//     const { presentsId } = req.params;
+//     try {
+//       const present = await Present.findById(presentsId)
+//       res.render('presentDetail', present);
+//   } catch (error) {
+//     next(error)  
+//   }   
+  })
 
+/* GET form view */
+/* ROUTE presents/new*/
+router.get('/new', function (req, res, next){
+    res.render('newPresent');
+  })
+
+/* POTS get users presents inputs. */
+/* ROUTE presents/news*/
+router.post('/new', async function (req, res, next) {
+  const { name, image, price, recipient, description } = req.body;
+  try {
+      const createdPresent = await Present.create({ name, image, price, recipient, description });
+      res.redirect(`/presents/${createdPresent._id}`);
+  } catch (error) {
+      next(error)
+  }
+}); 
+
+/* GET one presents page. */
+/* ROUTE presents/:presentsId */
 router.get('/:presentsId', async function (req, res, next){
     const { presentsId } = req.params;
     try {
@@ -21,6 +54,7 @@ router.get('/:presentsId', async function (req, res, next){
   } catch (error) {
     next(error)  
   }   
-  })
+  });
+  
 
 module.exports = router;
